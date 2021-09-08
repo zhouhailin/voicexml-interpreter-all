a3afb56b18cd7c2117c60f46487b23348e0ebcfe
zhouhailin <zhouhailin555@aliyun.com>
2021-09-08 20:33:12 +0800

    add code framework.

---
+++ voicexml-interpreter-spring-boot-example/src/main/java/link/thingscloud/voicexml/interpreter/VoicexmlInterpreterAllApplication.java
@@ -0,0 +1,13 @@
+package link.thingscloud.voicexml.interpreter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoicexmlInterpreterAllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoicexmlInterpreterAllApplication.class, args);
+    }
+
+}
---
+++ voicexml-interpreter-spring-boot-example/src/test/java/link/thingscloud/voicexml/interpreter/VoicexmlInterpreterAllApplicationTests.java
@@ -0,0 +1,13 @@
+package link.thingscloud.voicexml.interpreter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoicexmlInterpreterAllApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
